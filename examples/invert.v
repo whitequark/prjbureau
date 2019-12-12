12ca8e2db306611ea7381ea1c119931eede1e7b8
whitequark <whitequark@whitequark.org>
2019-12-12 07:02:43 +0000

    v2jed: encode options and pins as a part of source.

--- examples/invert.v
+++ examples/invert.v
@@ -1,3 +1,7 @@
-module invert(input I, output O);
+//OPT: -device TQFP44
+//PIN: CHIP "top" ASSIGNED TO TQFP44
+//PIN: I : 2
+//PIN: O : 3
+module top(input I, output O);
   INV n(.A(I), .QN(O));
 endmodule
--- v2jed.sh
+++ v2jed.sh
@@ -5,5 +5,7 @@ export WINEPREFIX=$HOME/.wine_atf
 export FITTERDIR=$(winepath -w ${ROOT}/vendor)
 
 NAME=$1; shift
+OPTS=$(grep '//OPT:' ${NAME}.v | cut -d' ' -f2-)
 yosys -p "read_verilog -lib ${ROOT}/cells.v; read_verilog ${NAME}.v" -o ${NAME}.edif
-wine ${FITTERDIR}\\fit1502.exe -i ${NAME}.edif -o ${NAME}.jed -strategy ifmt=edif $*
+grep '//PIN:' ${NAME}.v | cut -d' ' -f2- >${NAME}.pin
+wine ${FITTERDIR}\\fit1502.exe -i ${NAME}.edif -o ${NAME}.jed -strategy ifmt=edif ${OPTS}
