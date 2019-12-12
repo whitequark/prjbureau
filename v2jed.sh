1ad423b6f3ff633310017a41e9c317072a5d5a81
whitequark <whitequark@whitequark.org>
2019-12-12 07:30:05 +0000

    fuzzers: add basic IO fuzzers.

--- v2jed.sh
+++ v2jed.sh
@@ -8,4 +8,4 @@ NAME=$1; shift
 OPTS=$(grep '//OPT:' ${NAME}.v | cut -d' ' -f2-)
 yosys -p "read_verilog -lib ${ROOT}/cells.v; read_verilog ${NAME}.v" -o ${NAME}.edif
 grep '//PIN:' ${NAME}.v | cut -d' ' -f2- >${NAME}.pin
-wine ${FITTERDIR}\\fit1502.exe -i ${NAME}.edif -o ${NAME}.jed -strategy ifmt=edif ${OPTS}
+wine ${FITTERDIR}\\fit1502.exe -i ${NAME}.edif -o ${NAME}.jed -strategy ifmt=edif -strategy optimize=off -strategy JTAG=off ${OPTS}
---
+++ work/work.v
@@ -0,0 +1,6 @@
+//OPT: -device TQFP44
+//PIN: CHIP "top" ASSIGNED TO TQFP44
+//PIN: O : 8
+module top(output O);
+  assign O = 1'b1;
+endmodule
