700dd8ce7bf642e3a1200b768ae33440269c1d85
whitequark <whitequark@whitequark.org>
2020-08-10 20:02:57 +0000

    Fuzz GOE muxes: handle one signal in multiple muxes, as well as GND.
    The latter is necessary to get a complete routing matrix. The former
    is necessary to get a complete routing matrix on ATF1504 and larger.

--- fuzzers/020-goe_mux/fuzzer.py
+++ fuzzers/020-goe_mux/fuzzer.py
@@ -1,4 +1,5 @@
 import subprocess
+from collections import defaultdict
 
 from util import database, toolchain, bitdiff
 
@@ -13,21 +14,36 @@ with database.transact() as db:
         for goe_index, (goe_name, goe_mux) in enumerate(device['goe_muxes'].items()):
             goe_mux['fuses'] = list(range(goe_mux_range.start + goe_mux_size * goe_index,
                                           goe_mux_range.start + goe_mux_size * (goe_index + 1)))
+            goe_mux['values'] = {}
 
-        def run_pad(pad_name):
-            for other_macrocell_name, other_macrocell in device['macrocells'].items():
-                if other_macrocell['pad'] != pad_name and other_macrocell['pad'] in pinout:
-                    break
-            else:
-                assert False
+        def run_pads(pad_names):
+            ports = []
+            cells = []
+            pins  = {}
+
+            used_pads = set(pad_names)
+            for n, pad_name in enumerate(pad_names):
+                for other_macrocell_name, other_macrocell in device['macrocells'].items():
+                    if (other_macrocell['pad'] not in used_pads and
+                            other_macrocell['pad'] in pinout):
+                        used_pads.add(other_macrocell['pad'])
+                        break
+                else:
+                    assert False
+
+                ports += [f"input OE{n}", f"output O{n}"]
+                cells += [f"wire Q{n}; DFFAS dff{n}(1'b0, O{n}, 1'b0, Q{n}); ",
+                          f"TRI t{n}(Q{n}, OE{n}, O{n}); "]
+                pins.update({
+                    f"O{n}": pinout[other_macrocell['pad']],
+                    f"OE{n}": pinout[pad_name],
+                })
+
             return toolchain.run(
-                f"module top(input OE, output O); "
-                f"wire Q; DFFAS dff(1'b0, O, 1'b0, Q); TRI t(Q, OE, O); "
+                f"module top({', '.join(ports)}); "
+                f"{' '.join(cells)} "
                 f"endmodule",
-                {
-                    'O': pinout[other_macrocell['pad']],
-                    'OE': pinout[pad_name],
-                },
+                pins,
                 f"{device_name}-{package}")
 
         # So, GOE mux feedback inputs. Where do I start...
@@ -98,30 +114,82 @@ with database.transact() as db:
                 None,
                 f"{device_name}-{package}",
                 strategy={'logic_doubling':'on'},
-                format='tt',
-                name=f"{macrocell_name}-fb")
+                format='tt')
 
-        nodes = {}
+        def has_mux_value(node_name, fuses):
+            for mux_name, mux in device['goe_muxes'].items():
+                gnd_value = (1 << len(mux['fuses'])) - 1
+                value = sum(fuses[fuse] << n_fuse for n_fuse, fuse in enumerate(mux['fuses']))
+                if value == gnd_value: continue
 
+                if node_name in mux['values']:
+                    assert mux['values'][node_name] == value
+                    return True
+
+            return False
+
+        def add_mux_value(node_name, fuses, *, reserved=()):
+            for mux_name, mux in device['goe_muxes'].items():
+                if mux_name in reserved:
+                    continue
+
+                gnd_value = (1 << len(mux['fuses'])) - 1
+                value = sum(fuses[fuse] << n_fuse for n_fuse, fuse in enumerate(mux['fuses']))
+                if value == gnd_value: continue
+
+                assert node_name not in mux['values'], \
+                       f"GOE mux {mux_name} already has a value for {node_name}"
+                mux['values'][node_name] = value
+                return mux_name
+
+            assert False, f"all GOE muxes unprogrammed"
+
+        worklist = set()
         for node_type in ('clocks', 'enables', 'macrocells'):
             for node_name, node in device[node_type].items():
-                if node['pad'] not in pinout:
+                worklist.add(node['pad'])
+
+        depth = 0
+        pad_muxes = defaultdict(lambda: set())
+        mux_pads = defaultdict(lambda: set())
+        while worklist:
+            updates = []
+            for pad in worklist:
+                if pad not in pinout:
                     continue
+
+                reserved_muxes = []
+                reserved_pads = []
+                if depth > 0:
+                    for _, pad_mux_name in zip(range(depth), pad_muxes[pad]):
+                        reserved_muxes.append(pad_mux_name)
+                        for other_pad in mux_pads[pad_mux_name]:
+                            if other_pad == pad or other_pad in reserved_pads:
+                                continue
+                            if pad_muxes[other_pad].intersection(pad_muxes[pad]) != {pad_mux_name}:
+                                continue
+                            reserved_pads.append(other_pad)
+                            break
+                    if len(reserved_pads) != len(reserved_muxes):
+                        continue
+
                 try:
-                    fuses = run_pad(node['pad'])
+                    fuses = run_pads((pad, *reserved_pads))
                 except subprocess.CalledProcessError as err:
                     if err.returncode != 250:
                         raise
                     # Design constrained so that inability to use GOE is a total failure.
                     continue
-                nodes[f"{node['pad']}_PAD"] = fuses
 
-        def same_goe_fuses(fuses1, fuses2):
-            for mux_name, mux in device['goe_muxes'].items():
-                for fuse in mux['fuses']:
-                    if fuses1[fuse] != fuses2[fuse]:
-                        return False
-            return True
+                mux_name = add_mux_value(f"{pad}_PAD", fuses, reserved=reserved_muxes)
+                updates.append((pad, mux_name))
+
+            worklist.clear()
+            for pad, mux_name in updates:
+                worklist.add(pad)
+                pad_muxes[pad].add(mux_name)
+                mux_pads[mux_name].add(pad)
+            depth += 1
 
         for macrocell_idx, (macrocell_name, macrocell) in enumerate(device['macrocells'].items()):
             fuses = run_fb(macrocell_idx, macrocell_name)
@@ -129,8 +197,7 @@ with database.transact() as db:
                 # Cannot constrain design by using AS with CUPL (fitter always rejects), so instead
                 # verify that the only cell that should have sync feedback, has it.
                 continue
-            if (f"{macrocell['pad']}_PAD" in nodes and
-                    same_goe_fuses(nodes[f"{macrocell['pad']}_PAD"], fuses)):
+            if has_mux_value(f"{macrocell['pad']}_PAD", fuses):
                 # Due to a fitter bug, if a combinatorial node that is not itself a legal GOE
                 # driver is constrained using OE_node, and the pad of the macrocell where the node
                 # is placed, conversely, is a legal GOE driver, a miscompilation happens:
@@ -144,24 +211,15 @@ with database.transact() as db:
                 # To account for this bug, ignore any FB mux drivers if a PAD mux driver exists
                 # that refers to the same macrocell.
                 continue
-            nodes[f"{macrocell_name}_FB"] = fuses
+            add_mux_value(f"{macrocell_name}_FB", fuses)
 
-        # The code above discovers 80% of choices on ATF1502. Where did 5 more go? I don't know.
-        # It does not look like the fitter can route OE1 and OE2 anywhere other than GOE5 and GOE2,
-        # and I didn't check the rest.
-
-        found = 0
         for mux_name, mux in device['goe_muxes'].items():
-            gnd_value = (1 << len(mux['fuses'])) - 1
-            mux['values'] = {'GND': gnd_value}
-            for node, fuses in nodes.items():
-                value = sum(fuses[fuse] << n_fuse for n_fuse, fuse in enumerate(mux['fuses']))
-                if value == gnd_value: continue
-                # print(mux_name, node.ljust(8), "{:0{}b}".format(value, len(mux['fuses'])))
+            # Each GOE mux has exactly one fuse which is never used by the fitter. Hardware testing
+            # and celestial rituals demonstrate that this fuse drives the GOE network low.
+            assert len(mux['values']) == len(mux['fuses']) - 1, \
+                   f"GOE mux {mux_name} should have exactly one unused value"
+            erased_value = (1 << len(mux['fuses'])) - 1
+            for n_fuse in range(len(mux['fuses'])):
+                value = erased_value ^ (1 << n_fuse)
                 if value not in mux['values'].values():
-                    found += 1
-                mux['values'][node] = value
-
-        total = sum(len(mux['fuses']) for mux in device['goe_muxes'].values())
-        print("Fuzzed {}/{} ({:.0f}%) GOE muxes in device {}-{}"
-              .format(found, total, found / total * 100, device_name, package))
+                    mux['values']['GND'] = value
