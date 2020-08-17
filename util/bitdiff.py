5d53a82232d1b562302b6bf28a1e3b6cccd42aa2
whitequark <whitequark@whitequark.org>
2020-08-17 17:57:23 +0000

    Fuzz JTAG enable.

--- util/bitdiff.py
+++ util/bitdiff.py
@@ -3,12 +3,12 @@ from bitarray import bitarray
 
 def describe(expected, variants, *, scope=None):
     bitstreams = list(variants.values())
-    pairwise_xor = bitarray(bitstreams[0])
+    pairwise_xor = bitarray(bitstreams[0], endian=bitstreams[0].endian())
     pairwise_xor.setall(0)
     for a, b in zip(bitstreams, bitstreams[1:]):
         pairwise_xor |= a ^ b
     if scope is not None:
-        mask = bitarray(len(pairwise_xor))
+        mask = bitarray(len(pairwise_xor), endian=pairwise_xor.endian())
         mask.setall(0)
         for n in scope:
             mask[n] = 1
